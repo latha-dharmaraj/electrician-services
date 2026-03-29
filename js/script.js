bcd93b26d736fff0992591895d3f58d897767816
lathadharmaraj <lathadharmaraj24@gmail.com>
2026-03-29 17:35:52 +0530

    folder name changed

--- js/script.js
+++ js/script.js
@@ -4,13 +4,3 @@ document.querySelector(".fa-bars").addEventListener("click",function(){
 document.querySelector(".cancel-symbol").addEventListener("click",function(){
     document.querySelector(".side-nav-bar").classList.remove("nav-open");
 })
-
-document.querySelector(".about-page").addEventListener("click",function(){
-    window.location.href= "about.html";
-})
-document.querySelector(".home-page").addEventListener("click",function(){
-    window.location.href= "index.html";
-})
-document.querySelector(".contact-page").addEventListener("click", function(){
-    window.location.href= "contact-page.html";
-})  
